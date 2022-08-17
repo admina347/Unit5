9d6c1485d1109d382770a6e3fddedd956c3b427c
admina <it-mactep@mail.ru>
2022-08-17 18:45:25 +0500

    unit5-2-8-1 + show result

--- Program.cs
+++ Program.cs
@@ -1,34 +1,55 @@
 ﻿using System;
 
-static int[] GetArrayFromConsole() {
-  var result = new int[5];
+static int[] GetArrayFromConsole()
+{
 
-  for (int i = 0; i < result.Length; i++) {
-    Console.WriteLine("Введите элемент массива номер {0}", i + 1);
-    result[i] = int.Parse(Console.ReadLine());
-  }
+    var result = new int[5];
 
-    int temp = 0;
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+        result[i] = int.Parse(Console.ReadLine());
+    }
 
-		for (int i = 0; i < result.Length; i++)
-		{
-			for (int j = i + 1; j < result.Length; j++)
-			{
-				if (result[i] > result[j])
-				{
-					temp = result[i];
-					result[i] = result[j];
-					result[j] = temp;
-				}
-			}
-		}
-
-		foreach(var item in result)
-		{
-			Console.Write(item);
-		}
-
-  return result;
+    return result;
 }
 
-GetArrayFromConsole();
+static int[] SortArray(int[] result)
+{
+    int temp = 0;
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        for (int j = i + 1; j < result.Length; j++)
+        {
+            if (result[i] > result[j])
+            {
+                temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+            }
+        }
+    }
+
+    return result;
+}
+
+int[] array = GetArrayFromConsole();
+
+Console.WriteLine("Вы ввели: ");
+
+foreach (var item in array)
+{
+    Console.Write(item);
+}
+
+int[] sortarray = SortArray(array);
+
+Console.WriteLine("\nСортировка: ");
+
+foreach (var item in sortarray)
+{
+    Console.Write(item);
+}
+
+Console.ReadKey();
