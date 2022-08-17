54c70013c887d9d5d5b62888e6a2ce2b61b37521
admina <it-mactep@mail.ru>
2022-08-17 16:32:11 +0500

    Unit5-1-6

--- Program.cs
+++ Program.cs
@@ -1,19 +1,34 @@
-﻿static string ShowColor()
-{
-    return Console.ReadLine();
+﻿using System;
 
+static int[] GetArrayFromConsole() {
+  var result = new int[5];
 
-Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы: ");
+  for (int i = 0; i < result.Length; i++) {
+    Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+    result[i] = int.Parse(Console.ReadLine());
+  }
 
-var favcoloros = new string[3];
-for (int i = 0; i < favcoloros.Length ; i++ )
-{
-    favcoloros[i] = ShowColor();
+    int temp = 0;
+
+		for (int i = 0; i < result.Length; i++)
+		{
+			for (int j = i + 1; j < result.Length; j++)
+			{
+				if (result[i] > result[j])
+				{
+					temp = result[i];
+					result[i] = result[j];
+					result[j] = temp;
+				}
+			}
+		}
+
+		foreach(var item in result)
+		{
+			Console.Write(item);
+		}
+
+  return result;
 }
 
-Console.WriteLine("Ваши любимые цвета: ");
-foreach(var color in favcoloros)
-{
-    Console.WriteLine(color);
-}
-Console.ReadKey();
+GetArrayFromConsole();
