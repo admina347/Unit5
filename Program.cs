2eebd7748f25973bee570e0600ca18ed6c339fb9
admina <it-mactep@mail.ru>
2022-08-22 14:11:36 +0500

    unit5-3-13

--- Program.cs
+++ Program.cs
@@ -1,25 +1,92 @@
 ﻿using System;
 
-class MainClass {
-  public static void Main (string[] args) {
-		
-		var arr = new int[] { 1, 2,3 };
-		BigDataOperation(arr);
+static int[] GetArrayFromConsole()
+{
 
-		Console.WriteLine(arr[0]);
+    var result = new int[5];
 
-  }
+    for (int i = 0; i < result.Length; i++)
+    {
+        Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+        result[i] = int.Parse(Console.ReadLine());
+    }
 
+    return result;
+}
 
-	static void BigDataOperation(int[] arr)
-	{
-		arr[0] = 4;
-	}
+static void SortArray(in int[] array, out int[] sorteddesc, out int[] sortedasc)
+{
+    sortedasc = SortArrayAsc(array);
+    Console.WriteLine("\nСортировка массива от меньшего к большему: ");
+    foreach (var item in sortedasc)
+    {
+        Console.Write(item);
+    }
 
-static int SumNumbers(ref int num1, in int num2, out int num3, int num4) => num3 = num1 + num2;
-
-
-
-
+    sorteddesc = SortArrayDesc(array);
+    Console.WriteLine("\nСортировка массива от большего к меньшему: ");
+    foreach (var item in sorteddesc)
+    {
+        Console.Write(item);
+    }
+    
 
 }
+
+static int[] SortArrayAsc(int[] result)
+{
+    int temp = 0;
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        for (int j = i + 1; j < result.Length; j++)
+        {
+            if (result[i] > result[j])
+            {
+                temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+            }
+        }
+    }
+
+    return result;
+}
+
+static int[] SortArrayDesc(int[] result)
+{
+    int temp = 0;
+
+    for (int i = 0; i < result.Length; i++)
+    {
+        for (int j = i + 1; j < result.Length; j++)
+        {
+            if (result[i] < result[j])
+            {
+                temp = result[i];
+                result[i] = result[j];
+                result[j] = temp;
+            }
+        }
+    }
+
+    return result;
+}
+
+int[] array = GetArrayFromConsole();
+
+Console.WriteLine("Вы ввели: ");
+
+foreach (var item in array)
+{
+    Console.Write(item);
+}
+
+int[] desc, asc;
+SortArray(array, out desc, out asc);
+
+//Console.WriteLine("\nСортировка: ");
+
+
+
+Console.ReadKey();
